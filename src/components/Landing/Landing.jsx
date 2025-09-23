f4719beed9ea5a4e3ae559cb323d38f7693f1f38
raworiginal <randall.a.wolfe@gmail.com>
2025-09-23 08:08:54 -0700

    made the landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,12 +1,25 @@
-// src/components/Landing.jsx
-
+import { Link } from "react-router";
 const Landing = () => {
-  return (
-    <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <p>Sign up now, or sign in to see your super secret dashboard!</p>
-    </main>
-  );
+	return (
+		<main>
+			<div className="hero glass bg-secondary">
+				<div className="hero-content text-center">
+					<div className="max-w-md p-2 bg-primary glass rounded-box">
+						<h1 className="text-9xl font-bold text-primary-content rampart">
+							POP 5!
+						</h1>
+						<p className="py-6"></p>
+						<Link className="btn" to={"/sign-in"}>
+							Login
+						</Link>
+						<Link className="btn btn-info" to={"/sign-up"}>
+							Signup
+						</Link>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Landing;
